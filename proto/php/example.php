7c33387f8bd189645516d97e7bb40f6497ec6d65
kzbandai <nisigin@gmail.com>
2017-10-12 20:02:14 +0900

    Rename

---
+++ proto/php/example.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Foo\Bar\SearchRequest;
+
+$foo = new SearchRequest();
+$foo->setQuery('hogehoge fugafuga');
+$foo->setPageNumber(100);
+
+echo $foo->serializeToString();

2556ad1140df540f66318e3b790dabed75c9c721
kzbandai <nisigin@gmail.com>
2017-12-25 21:52:42 +0900

    Test basic codes

--- scala/src/main/scala/HelloWorld.scala
+++ scala/src/main/scala/HelloWorld.scala
@@ -1,5 +1,31 @@
 object HelloWorld {
+  var num: Int = 1
+  var str: String = "start"
+  var short: Short = _ // initial value
+
+  var NUM: Int = 1
+  val STR: String = "2"
+  var array: Array[String] = Array("a", "b", "c")
+
   def main(args: Array[String]) = {
     println("Hello World")
+
+    println(num)
+    println(str)
+    println(short)
+
+    println(NUM)
+    println(STR)
+
+    if (true) println(STR) else println("else")
+
+    for (s <- array) println(s)
+
+    val one = 1
+    one match {
+      case 1 => println("one")
+      case 2 => println("two")
+      case _ => println("other")
+    }
   }
 }

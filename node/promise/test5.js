832df670bebd2f66c3f7406271cb63b0549d4c2e
kzbandai <nisigin@gmail.com>
2020-03-18 01:36:47 +0900

    Add node Promise code

--- node/promise/test5.js
+++ node/promise/test5.js
@@ -1,8 +1,9 @@
 function taskA() {
     console.log("Task A");
+    throw new Error("throw Error @ Task A");
 }
 function taskB() {
-    console.log("Task B");
+    console.log("Task B"); // isn't called
 }
 function onRejected(error) {
     console.log("Catch Error: A or B", error);

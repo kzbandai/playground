49f1c8ae1664c500c58b5b4161e04e1a33c1d922
kzbandai <nisigin@gmail.com>
2020-06-03 00:42:24 +0900

    cpp

--- cpp/samples/main.cpp
+++ cpp/samples/main.cpp
@@ -51,18 +51,6 @@ int main() {
     // ラムダ式を関数呼び出し
     []() {}();
 
-    // 関数、返り値の型を文頭で宣言する
-    int plus(int x, int y) {
-        return x + y;
-    }
-
-    void f() {
-        // OK
-    }
-
-    // autoも使える
-    auto d() { return ""s; }
-
     // 空文も書ける
     ;
 
@@ -73,4 +61,57 @@ int main() {
     }
     // 複合文終了、セミコロンはいらない
     // ブロックスコープは有効
+
+    if (2 < 1)
+        std::cout << "sentence 1.\n"; // 文1, 実行されない
+    std::cout << "sentence 2.\n"; // 文2
+
+    auto r = ""s;
+
+    if ("aa"s < "ab"s) {
+        r = "aa\n"s;
+    } else {
+        r = "ab\n"s;
+    }
+
+    // 小さい方の文字が出力される
+    std::cout << r;
+
+    // bool
+    std::cout << std::boolalpha;
+    std::cout << true << "\n"s << false << "\n";
+    std::cout << std::noboolalpha;
+    std::cout << true << "\n"s << false << "\n"
+
+    // 短絡評価
+    auto a = []() {
+        std::cout << "a\n"s;
+        return false;
+    };
+    auto b = []() {
+        std::cout << "b\n"s;
+        return true;
+    };
+
+    bool c = a() && b();
+    std::cout << std::boolalpha << c;;
+
+    // boolのint変換
+    // 1
+    int True = true;
+    // 0
+    int False = false;
 }
+
+// 関数、返り値の型を文頭で宣言する
+int plus(int x, int y) {
+    return x + y;
+}
+
+
+void f() {
+    // OK
+}
+
+// autoも使える
+auto d() { return ""s; }
